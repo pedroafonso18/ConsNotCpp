488ee8fcb164077d9e0802e7911b993f22ce49dd
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-30 16:51:25 -0300

    feat: Added D_Insert

---
+++ include/D_Insert.h
@@ -0,0 +1,16 @@
+#ifndef D_INSERT_H
+#define D_INSERT_H
+#include "Database.h"
+
+class Insert {
+private:
+    std::unique_ptr<Database> ownedDb;
+    Database* db;
+public:
+    explicit Insert(Database& db) : db(&db) {}
+    explicit Insert(std::string Url)
+        : ownedDb(std::unique_ptr<Database>(new Database(Url))), db(ownedDb.get()) {}
+    void insertConsultaLog(std::string cpf, std::string saldo, std::string aviso, std::string login, std::string nome, std::string numero, bool erro);
+    void updateConsultado(std::string cpf);
+};
+#endif //D_INSERT_H
---
+++ src/D_Insert.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Usuario on 30/07/2025.
+//
