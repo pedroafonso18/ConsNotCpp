db7028c150d4dfaf10e1ec05fbbd64d70ce9abc1
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-07-31 14:06:46 -0300

    feat: Finished D_Fetch, C++ is cool

--- include/D_Fetch.h
+++ include/D_Fetch.h
@@ -3,6 +3,7 @@
 #include "Database.h"
 #include "Models.h"
 #include "Utils.h"
+#include <memory>
 
 class Fetch {
 private:
@@ -12,11 +13,11 @@ public:
     explicit Fetch(Database& db) : db(&db) {}
     explicit Fetch(std::string Url)
         : ownedDb(std::unique_ptr<Database>(new Database(Url))), db(ownedDb.get()) {}
-    Models::Logins* countConsultas(std::string user1, std::string user2, std::string user3, std::string user4);
-    Models::Pessoa* fetchConsultas(const Database* stormClient, std::string cpf);
-    std::string fetchCurrentCampaign();
-    bool isPaused();
-    std::vector<std::string> fetchCustomers();
+    std::unique_ptr<Models::Logins> countConsultas(std::string user1, std::string user2, std::string user3, std::string user4) const;
+    std::unique_ptr<Models::Pessoa> fetchConsultas(const Database* stormClient, std::string cpf);
+    std::string fetchCurrentCampaign() const;
+    bool isPaused() const;
+    std::vector<std::string> fetchCustomers() const;
 };
 
 #endif //D_FETCH_H
--- src/D_Fetch.cpp
+++ src/D_Fetch.cpp
@@ -1,8 +1,10 @@
 #include "../include/D_Fetch.h"
 
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 
-Models::Pessoa *Fetch::fetchConsultas(const Database *stormClient, std::string cpf) {
+std::unique_ptr<Models::Pessoa> Fetch::fetchConsultas(const Database *stormClient, std::string cpf) {
     auto c = db->getConnection();
     auto c_storm = stormClient->getConnection();
     if (!c->is_open() || !c_storm->is_open()) {
@@ -15,7 +17,7 @@ Models::Pessoa *Fetch::fetchConsultas(const Database *stormClient, std::string c
     pqxx::work transaction(*c);
     pqxx::work storm_transaction(*c_storm);
 
-    Models::Pessoa* pessoa = new Models::Pessoa();
+    auto pessoa = std::unique_ptr<Models::Pessoa>(new Models::Pessoa());
 
     std::string query = "SELECT nome, numero FROM contatos WHERE cpf is not null AND (cpf = $1 OR cpf = $2)";
     std::cout << "Executing query: " << query << std::endl;
@@ -48,14 +50,97 @@ Models::Pessoa *Fetch::fetchConsultas(const Database *stormClient, std::string c
         }
 
         std::cout << "Nenhum contato encontrado para o CPF: " << cpf << " dentro da Storm." << std::endl;
-        delete pessoa;
         transaction.commit();
         storm_transaction.commit();
         return nullptr;
     } catch (pqxx::sql_error const &e) {
         transaction.abort();
         storm_transaction.abort();
-        delete pessoa;
         throw std::runtime_error("Database error: " + std::string(e.what()));
     }
 }
+
+std::unique_ptr<Models::Logins> Fetch::countConsultas(std::string user1, std::string user2, std::string user3, std::string user4) const {
+    const auto c = db->getConnection();
+    if (!c->is_open()) {
+        std::clog << "ERROR: db connection is not open.\n";
+        throw std::runtime_error("DB CONNECTION IS NOT ACTIVE");
+    }
+    pqxx::work transaction(*c);
+
+    auto logins = std::unique_ptr<Models::Logins>(new Models::Logins());
+    try {
+        const auto result = transaction.exec("SELECT COUNT(CASE WHEN login = $1 THEN 1 END) as login1, COUNT(CASE WHEN login = $2 THEN 1 END) as login2, COUNT(CASE WHEN login = $3 THEN 1 END) as login3, COUNT(CASE WHEN login = $4 THEN 1 END) as login4 FROM logs_consultas WHERE created_at >= NOW() - INTERVAL '24 hours'", user1, user2, user3, user4);
+        if (!result.empty()) {
+            logins->login1 = result[0][0].as<int>();
+            logins->login2 = result[0][1].as<int>();
+            logins->login3 = result[0][2].as<int>();
+            logins->login4 = result[0][3].as<int>();
+            transaction.commit();
+            return logins;
+        }
+
+        transaction.commit();
+        return nullptr;
+    } catch (pqxx::sql_error const& e) {
+        transaction.abort();
+        throw std::runtime_error("Database error: " + std::string(e.what()));
+    }
+}
+
+std::string Fetch::fetchCurrentCampaign() const {
+    auto c = db->getConnection();
+    if (!c->is_open()) {
+        std::clog << "ERROR: db connection is not open.\n";
+        throw std::runtime_error("DB CONNECTION IS NOT ACTIVE");
+    }
+
+    try {
+        pqxx::work transaction(*c);
+        const auto result = transaction.exec("SELECT campanha_ativa FROM config LIMIT 1");
+        std::string campanha = result.one_field().as<std::string>();
+        transaction.commit();
+        return campanha;
+    } catch (const pqxx::sql_error& e) {
+        throw std::runtime_error("Database error: " + std::string(e.what()));
+    }
+}
+
+bool Fetch::isPaused() const {
+    auto c = db->getConnection();
+    if (!c->is_open()) {
+        std::clog << "ERROR: db connection is not open.\n";
+        throw std::runtime_error("DB CONNECTION IS NOT ACTIVE");
+    }
+
+    try {
+        pqxx::work transaction(*c);
+        const auto result = transaction.exec("SELECT pausado FROM config");
+        transaction.commit();
+        return result.one_field().as<bool>();
+    } catch (const pqxx::sql_error& e) {
+        throw std::runtime_error("Database error: " + std::string(e.what()));
+    }
+}
+
+std::vector<std::string> Fetch::fetchCustomers() const {
+    auto c = db->getConnection();
+    auto campanha = fetchCurrentCampaign();
+    if (!c->is_open()) {
+        std::clog << "ERROR: db connection is not open.\n";
+        throw std::runtime_error("DB_CONNECTION IS NOT ACTIVE");
+    }
+
+    try {
+        std::vector<std::string> vec;
+        pqxx::work transaction(*c);
+        const auto result = transaction.exec("SELECT cpf FROM consultar WHERE consultado = false AND campanha = $1 LIMIT 10", campanha);
+        for (auto row : result) {
+            vec.push_back(row[0].as<std::string>());
+        }
+        return vec;
+    } catch (const pqxx::sql_error& e) {
+        throw std::runtime_error("Database error: " + std::string(e.what()));
+    }
+}
+
